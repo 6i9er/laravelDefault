5fbf96a137e39af0d98868146c5f9532a880eeae
mina <eng.mina23@gmail.com>
2018-04-03 13:52:23 +0200

    fix bug in error 404

--- app/Helpers/PathHelpers.php
+++ app/Helpers/PathHelpers.php
@@ -3,8 +3,13 @@ function getUrlPath($urlPath){
     return url(''). "/" . App::getLocale() . "/" .$urlPath;
 }
 function getPageNameForTitle($hasParameter = '0' , $titleName = ''){
+//    return count(explode( url(App::getLocale()), url()->current() ));
     if($hasParameter == '0') {
-        return str_replace("/" , "" , explode( url(App::getLocale()), url()->current() )[1] );
+        if(count(explode( url(App::getLocale()), url()->current() )) > 1){
+            return str_replace("/" , "" , explode( url(App::getLocale()), url()->current() )[1] );
+        }else{
+            return "Wrong Page";
+        }
     }else {
         return $titleName;
     }
